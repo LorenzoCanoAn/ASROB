983b1f64b7bdd7fc4aedaf14d37b3efd156ea694
Carlos <carlos.tiernojimenez@gmail.com>
2021-06-04 12:08:26 +0200

    #esqueleton

--- FA/admittance.m
+++ FA/admittance.m
@@ -1,79 +1,77 @@
-% script: admittance.m
-% Admittance Control for a leg 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-close all;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  CONTROL PARAMETERS: SELECT THE PARAMETERS FRO YOUR EXERCISE
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%  EXTERNAL INPUTS
-help_op=-1;     % helping (+1) force/ oppositng force (-1)
-
-% WALKING AMPLITUDE
-walk=1;       % walking amplitude
-
-% ADMITTANCE
-h_stiff=5;      % low admittance
-l_stiff=2;      % high admittance
-
-% ASSISTANCE
-assis=0.5;      % total assistance [0..1]
-emg_ratio=0;  % proportion [0..1] of EMG force (latent force) with respect to the pattern
-
-%EXTERNAL FORCE
-ext=0.2;       % external force/torque applied
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   LEG MODEL AS AN ARTICULATED MECHANISM
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-L1 = Revolute('a',0,'alpha',pi/2,'qlim',[-pi/2 0]); % DH parameters for 0T1
-L2s = Revolute('a',0,'alpha',pi/2,'offset',pi/2);
-L2 = Revolute('a',2,'alpha',pi/2,'qlim',[-pi 0],'offset',-pi);
-L3 = Revolute('a',2,'alpha',0,'qlim',[pi 2*pi]);
-L4s = Revolute('a',0,'alpha',pi/2,'qlim',[0 pi],'offset',pi/2);
-L4 = Revolute('a',1,'alpha',0,'qlim',[0 pi],'offset',pi/2);
-izq_leg=SerialLink([L1,L2s,L2,L3,L4s,L4],'name', 'izqda_leg');
-qr=[0, 0, 0, 0, 0,0];
-izq_leg.base=transl(0,2,0);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%38.6%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear;
+close all;
+
+hip_rot = Revolute('a',0,'alpha',pi/2,'qlim',[0 pi/2]); % DH parameters for 0T1
+hip_ad = Revolute('a',0,'alpha',pi/2,'qlim',[-pi/2,pi],'offset',pi/2);
+hip_flx = Revolute('a',2,'alpha',pi/2,'qlim',[-pi 0],'offset',-pi);
+
+knee = Revolute('a',2,'alpha',0,'qlim',[pi 2*pi]);
+
+ankle_inv = Revolute('a',0,'alpha',-pi/2,'qlim',[-pi/2 30*pi/180], 'offset',pi/2);
+ankle_ext = Revolute('a',1,'alpha',0,'qlim',[-pi/2 pi/2],'offset',pi/2);
+
+izq_leg=SerialLink([hip_rot,hip_ad,hip_flx,knee,ankle_inv,ankle_ext],'name', 'izqda_leg');
+qr=[0, 0, 0, 0, 0, 0];
+izq_leg.base=transl(0,1,0)*trotz(-pi/2);
+
 izq_leg.plot(qr)
 teach(izq_leg,qr)
+
+%%
+
 hold on;
-L1 = Revolute('a',0,'alpha',pi/2,'qlim',[-pi/2 0]); % DH parameters for 0T1
-L2s = Revolute('a',0,'alpha',pi/2,'offset',pi/2);
-L2 = Revolute('a',2,'alpha',pi/2,'qlim',[-pi 0],'offset',-pi);
-L3 = Revolute('a',2,'alpha',0,'qlim',[pi 2*pi]);
-L4 = Revolute('a',1,'alpha',0,'qlim',[0 pi],'offset',pi/2);
-dcha_leg=SerialLink([L1,L2s,L2,L3,L4s,L4],'name', 'dcha_leg');
-qr=[0, 0, 0, 0, 0,0];
-dcha_leg.base=transl(0,-2,0);
+hip_rot = Revolute('a',0,'alpha',pi/2,'qlim',[-pi/2 0],'offset', 0); % DH parameters for 0T1
+hip_ad = Revolute('a',0,'alpha',pi/2,'qlim',[-pi/2,pi],'offset',pi/2);
+hip_flx = Revolute('a',2,'alpha',pi/2,'qlim',[-pi 0],'offset',-pi);
+
+ankle_inv = Revolute('a',0,'alpha',-pi/2,'qlim',[-pi/2 30*pi/180], 'offset',pi/2);
+ankle_ext = Revolute('a',1,'alpha',0,'qlim',[-pi/2 pi/2],'offset',pi/2);
+
+dcha_leg=SerialLink([hip_rot,hip_ad,hip_flx,knee,ankle_inv,ankle_ext],'name', 'dcha_leg');
+qr=[0, 0, 0, 0, 0, 0];
+dcha_leg.base=transl(0,-1,0)*trotz(-pi/2);
 dcha_leg.plot(qr)
 teach(dcha_leg,qr)
 
+%%
+
+shoulder_rot = Revolute('a',0,'alpha',-pi/2,'qlim',[-pi/2 pi/2],'offset',0); % DH parameters for 0T1
+shoulder_flx = Revolute('a',0,'alpha',pi/2,'qlim',[0 ,pi],'offset',pi/2);
+shoulder_ad = Revolute('a',2,'alpha',pi/2,'qlim',[-pi 0],'offset',0);
+
+elbow_pron  = Revolute('a',0,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
+elbow_flex  = Revolute('a',2,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
+
+wrist_flex  = Revolute('a',0,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
+wrist_abd   = Revolute('a',1,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+izq_arm=SerialLink([shoulder_rot,shoulder_flx,shoulder_ad,elbow_pron,elbow_flex,wrist_flex,wrist_abd],'name', 'izqda_arm');
+qr=[0,0,0,0,0,0,0];
+izq_arm.base=transl(0,1.5,5)*trotz(pi/2);
+izq_arm.plot(qr)
+teach(izq_arm,qr)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  SIMULINK ADMITTANCE CONTROL MODEL
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-open('admittance_control.slx')
-disp('INIT CONTROL: SIMULATE THE MOTION (Run in Simulink window)')
-disp('WHEN CONTROL SIMULATION FINISH, PRESS ENTER')
-pause
-disp('MOTION STARTED')
+%%
+shoulder_rot = Revolute('a',0,'alpha',-pi/2,'qlim',[-pi/2 pi/2],'offset',0); % DH parameters for 0T1
+shoulder_flx = Revolute('a',0,'alpha',pi/2,'qlim',[0 ,pi],'offset',pi/2);
+shoulder_ad = Revolute('a',2,'alpha',pi/2,'qlim',[-pi 0],'offset',0);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  SIMULINK ADMITTANCE CONTROL MODEL
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+elbow_pron  = Revolute('a',0,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
+elbow_flex  = Revolute('a',2,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
 
-for i = 1:(max(size(out.hip))-1)
-    izq_leg.animate([0,out.hip(i),out.knee(i),0]);
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+wrist_flex  = Revolute('a',0,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
+wrist_abd   = Revolute('a',1,'alpha',pi/2,'offset',0,'qlim',[-pi/2 pi/2]);
 
 
+dcha_arm=SerialLink([shoulder_rot,shoulder_flx,shoulder_ad,elbow_pron,elbow_flex,wrist_flex,wrist_abd],'name', 'dcha_arm');
+qr=[0,0,0,0,0,0,0];
+dcha_arm.base=transl(0,-1.5,5)*trotz(pi/2);
+dcha_arm.plot(qr)
+teach(dcha_arm,qr)
 
